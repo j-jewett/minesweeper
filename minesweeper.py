80def2150ce3e889b4390a85667dadd556f19255
j-jewett <jordan.jewett@ymail.com>
2023-05-11 14:40:14 -0400

    Completed private grid generation

--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,19 @@
+import random 
+
+# GLOBAL VARIABLES
+
 height = 0
 width = 0
 area = 0
 mine_limit = 0
 mines = 0
+public_grid = []
+private_grid = []
 
-def getInput():
+# FUNCTIONS
+
+# RETRIEVE GAME SETTINGS FROM USER
+def getGameSettings():
     global area
     global mine_limit
     getHeight()
@@ -50,6 +59,102 @@ def getMines():
         except TypeError:
             print("Invalid entry. Please enter an integer between 1-{0}.".format(mine_limit))
 
-getInput()
+# CREATE PUBLIC AND PRIVATE GAME GRID
+def createGame(height, width, mines):
+    global public_grid
+    global private_grid
+    public_grid = [["_" for col in range(width)] for row in range(height)]
+    private_grid = [["_" for col in range(width)] for row in range(height)]
 
+    # POPULATES GRID 
+    generateMines(private_grid, mines)
+    generateNumbers(private_grid)
+    
+# RANDOM MINE GENERATION
+def generateMines(grid, mines):
+    temp_mines = mines
+    while temp_mines > 0:
+        for row in grid:
+            for cell in range(0, len(row)):
+                if row[cell] != "x" and random.randint(1, area) == 1:
+                    row[cell] = "x"
+                    temp_mines -= 1
+
+
+# ITERATES THROUGH PRIVATE GRID, CALLS ADJACENT COUNTER TO POPULATE GRID
+def generateNumbers(grid):
+    for row in range(0, len(grid)):
+        for col in range(0, len(grid[0])):
+            adjacentCounter(grid, row, col)            
+        
+            
+
+# COUNTS ADJACENT MINES AND POPULATES CELL WITH CORRECT NUMBER
+def adjacentCounter(grid, row, col):
+    adjacentMines = 0
+    #inBounds = row - 1 >= 0 and col - 1 >= 0 and row + 1 <= len(grid) - 1 and col + 1 <= len(grid[0]) - 1
+    if grid[row][col] == "x":
+        return
+    try:
+        if grid[row - 1][col] == "x" and row - 1 >= 0 and grid[row][col] != "x":
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row - 1][col - 1] == "x" and row - 1 >= 0 and col - 1 >= 0 and grid[row][col]:
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row - 1][col + 1] == "x" and row - 1 >= 0:
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row][col - 1] == "x" and col - 1 >= 0:
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row + 1][col - 1] == "x" and col - 1 >= 0:
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row + 1][col + 1] == "x":
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row][col + 1] == "x":
+            adjacentMines += 1
+    except:
+        pass
+    try:
+        if grid[row + 1][col] == "x":
+            adjacentMines += 1
+    except:
+        pass
+    grid[row][col] = str(adjacentMines)
+        
+    
+
+            
+    
+    
+
+# RETRIEVE GAME SETTINGS FROM USER
+getGameSettings()
+
+# CREATE GAME WITH SETTING
+createGame(height, width, mines)
+
+
+# PUBLIC GRID TEST
+for row_line in public_grid:
+        print(row_line)
+
+# PRIVATE GRID TEST
+for row_line in private_grid:
+   print(row_line)
 
